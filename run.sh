a2fa50c3e3abce3885a9179b9c454c2eabfd7aef
Borxa Varela <borxa@varelabouzas.net>
2018-01-04 20:56:24 +0100

    Postgresql 9

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --name liferay6-db -e POSTGRES_DB=lportal -e POSTGRES_USER=liferay -e POSTGRES_PASSWORD=liferay --publish 5433:5432 postgres
+docker run --name liferay6-db -e POSTGRES_DB=lportal -e POSTGRES_USER=liferay -e POSTGRES_PASSWORD=liferay --publish 5433:5432 postgres:9
 docker run --name initial --link liferay6-db:postgres --publish 8080:8080 --volume ~/docker/liferay6/deploy:/usr/local/liferay-portal-6.2-ce-ga6/deploy borxa/liferay6-single:latest
